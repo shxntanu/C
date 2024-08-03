f6949517110ea6dbd0467790e3d99c5398f3b7d4
Shantanu <shantanuwable2003@gmail.com>
2024-08-04 00:54:04 +0530

    Complete singly linked list

--- singly_linked_list/sll.c
+++ singly_linked_list/sll.c
@@ -1,3 +1,66 @@
 #include "sll.h"
 #include <stdlib.h>
 
+#define IMPLEMENT_SLL(T)                                                         \
+    sll_##T *sll_create()                                                        \
+    {                                                                            \
+        sll_##T *list = (sll_##T *)malloc(sizeof(sll_##T));                      \
+        if (list)                                                                \
+        {                                                                        \
+            list->head = NULL;                                                   \
+            list->size = 0;                                                      \
+        }                                                                        \
+        return list;                                                             \
+    }                                                                            \
+                                                                                 \
+    void sll_##T##_push_front(sll_##T *list, T value)                            \
+    {                                                                            \
+        sll_node_##T *new_node = (sll_node_##T *)malloc(sizeof(sll_node_##T *)); \
+        if (new_node)                                                            \
+        {                                                                        \
+            new_node->data = value;                                              \
+            new_node->next = list->head;                                         \
+            list->head = new_node;                                               \
+            list->size++;                                                        \
+        }                                                                        \
+    }                                                                            \
+                                                                                 \
+    T sll_##T##_pop_front(sll_##T *list)                                         \
+    {                                                                            \
+        if (list->head)                                                          \
+        {                                                                        \
+            sll_node_##T *temp = list->head;                                     \
+            T value = temp->value;                                               \
+            list->head = temp->next;                                             \
+            free(temp);                                                          \
+            list->size--;                                                        \
+            return value;                                                        \
+        }                                                                        \
+    }                                                                            \
+                                                                                 \
+    void sll_##T##_free(sll_##T *list)                                           \
+    {                                                                            \
+        while (list->head)                                                       \
+        {                                                                        \
+            sll_node_##T *temp = list->head;                                     \
+            list->head = temp->next;                                             \
+            free(temp);                                                          \
+        }                                                                        \
+        free(list);                                                              \
+    }                                                                            \
+                                                                                 \
+    size_t sll_##T##_size(sll_##T *list)                                         \
+    {                                                                            \
+        return list->size;                                                       \
+    }                                                                            \
+                                                                                 \
+    int slist_##T##_is_empty(slist_##T *list)                                    \
+    {                                                                            \
+        return list->size == 0;                                                  \
+    }
+
+/**
+ * Usage in main program:
+ * IMPLEMENT_SLIST(int)
+ * IMPLEMENT_SLIST(double)
+ */
