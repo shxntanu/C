dbc126c1aa2ea7f4957201e4f2825af11635c6d8
Shantanu <shantanuwable2003@gmail.com>
2024-08-05 22:36:32 +0530

    update

--- singly_linked_list/sll.h
+++ singly_linked_list/sll.h
@@ -3,19 +3,13 @@
 
 #include <stddef.h>
 
+// Define a macro for creating a type-generic singly linked list
 #define DEFINE_SLL(T)                                                          \
-  /**                                                                          \
-   * Initialize a linked-list node of type T                                   \
-   */                                                                          \
-  typedef sll_node_##T {                                                       \
-    \ T data;                                                                  \
-    struct sll_node_##T *next                                                  \
-  }                                                                            \
-  sll_node_##T;                                                                \
+  typedef struct sll_node_##T {                                                \
+    T data;                                                                    \
+    struct sll_node_##T *next;                                                 \
+  } sll_node_##T;                                                              \
                                                                                \
-  /**                                                                          \
-   * Initialize a Singly Linked List with data type T                          \
-   */                                                                          \
   typedef struct {                                                             \
     sll_node_##T *head;                                                        \
     size_t size;                                                               \
@@ -23,9 +17,9 @@
                                                                                \
   sll_##T *sll_##T##_create();                                                 \
   void sll_##T##_push_front(sll_##T *list, T value);                           \
-  T slist_##T##_pop_front(slist_##T *list);                                    \
-  void slist_##T##_free(slist_##T *list);                                      \
-  size_t slist_##T##_size(slist_##T *list);                                    \
-  int slist_##T##_is_empty(slist_##T *list);
+  T sll_##T##_pop_front(sll_##T *list);                                        \
+  void sll_##T##_free(sll_##T *list);                                          \
+  size_t sll_##T##_size(sll_##T *list);                                        \
+  int sll_##T##_is_empty(sll_##T *list);
 
 #endif // SLL_H
