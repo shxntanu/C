358ecc2bcbcbf3569bebc2ae6b10077bd008f250
Shantanu <shantanuwable2003@gmail.com>
2024-08-05 23:30:23 +0530

    Add SLL switch case function in `sll.c`

--- singly_linked_list/sll.c
+++ singly_linked_list/sll.c
@@ -1,9 +1,9 @@
 #include "sll.h"
+#include <stdio.h>
 #include <stdlib.h>
 
-/**
- * Implement a singly linked list with the given type T
- */
+#define MAX_INPUT 100
+
 #define IMPLEMENT_SLL(T)                                                       \
   sll_##T *sll_##T##_create() {                                                \
     sll_##T *list = (sll_##T *)malloc(sizeof(sll_##T));                        \
@@ -49,3 +49,54 @@
   size_t sll_##T##_size(sll_##T *list) { return list->size; }                  \
                                                                                \
   int sll_##T##_is_empty(sll_##T *list) { return list->size == 0; }
+
+DEFINE_SLL(int)
+IMPLEMENT_SLL(int)
+
+void sll_int_menu() {
+  sll_int *list = sll_int_create();
+  char input[MAX_INPUT];
+  int choice, value;
+
+  while (1) {
+    printf("\nSingly Linked List (Int) Operations\n");
+    printf("1. Push front\n");
+    printf("2. Pop front\n");
+    printf("3. Print size\n");
+    printf("4. Check if empty\n");
+    printf("0. Back to main menu\n");
+    printf("Enter your choice: ");
+
+    fgets(input, MAX_INPUT, stdin);
+    choice = atoi(input);
+
+    switch (choice) {
+    case 1:
+      printf("Enter value to push: ");
+      fgets(input, MAX_INPUT, stdin);
+      value = atoi(input);
+      sll_int_push_front(list, value);
+      printf("Pushed %d to the front of the list.\n", value);
+      break;
+    case 2:
+      if (!sll_int_is_empty(list)) {
+        value = sll_int_pop_front(list);
+        printf("Popped %d from the front of the list.\n", value);
+      } else {
+        printf("The list is empty.\n");
+      }
+      break;
+    case 3:
+      printf("Size of the list: %zu\n", sll_int_size(list));
+      break;
+    case 4:
+      printf("Is the list empty? %s\n", sll_int_is_empty(list) ? "Yes" : "No");
+      break;
+    case 0:
+      sll_int_free(list);
+      return;
+    default:
+      printf("Invalid choice. Please try again.\n");
+    }
+  }
+}
